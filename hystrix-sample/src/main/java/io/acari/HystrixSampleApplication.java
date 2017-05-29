69f8624f634226ea06102f4a40647b2ca647c0a7
cyclic-reference <alex@cyclicreference.space>
2017-05-29 11:47:23 -0500

    Added Spring's Hystrix dashboard, thanks!

--- hystrix-sample/src/main/java/io/acari/HystrixSampleApplication.java
+++ hystrix-sample/src/main/java/io/acari/HystrixSampleApplication.java
@@ -2,7 +2,11 @@ package io.acari;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
+@EnableHystrix
 @SpringBootApplication
 public class HystrixSampleApplication {
 
