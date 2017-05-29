7255487fcc3271e0009f330f4da579038f7a50b2
cyclic-reference <alex@cyclicreference.space>
2017-05-29 09:59:38 -0500

    Inital Commit

---
+++ hystrix-sample/src/main/java/io/acari/HystrixSampleApplication.java
@@ -0,0 +1,12 @@
+package io.acari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixSampleApplication.class, args);
+	}
+}
