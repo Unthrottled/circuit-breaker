e2d38e6fb9fa20f2a851f6027932914f1dc15193
cyclic-reference <alex@cyclicreference.space>
2017-06-03 20:48:16 -0500

    I think this is a gewd session id api

--- hystrix-sample/angular-project/js/components/session.module.js
+++ hystrix-sample/angular-project/js/components/session.module.js
@@ -2,22 +2,20 @@
 
 var app = angular.module('myapp');
 
-app.service('sessionService', ["$q", "$http", "hostService", function ($q, $http, hostService) {
+app.service('sessionService', ["$http", "hostService", 'rx', function ($http, hostService, rx) {
     var self = this;
     self.NO_SESSION_ID = "LOL NOPE";
     self.sessionId = self.NO_SESSION_ID;
     return {
         getSessionId: function () {
             if (self.sessionId === self.NO_SESSION_ID) {
-                return $http.get(hostService + 'get/stream-id')
-                    .then(function (response) {
+                return rx.Observable.fromPromise($http.get(hostService + 'get/stream-id'))
+                    .map(function (response) {
                         self.sessionId = response;
                         return self.sessionId;
-                    })
+                    });
             } else {
-                return $q.defer()
-                    .resolve(self.sessionId)
-                    .promise;
+                return rx.Observable.of(self.sessionId);
             }
         }
     }
