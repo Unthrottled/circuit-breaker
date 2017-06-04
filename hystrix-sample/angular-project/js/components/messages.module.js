1222d77b946c9dd23dcd0a1549072028accda19c
cyclic-reference <alex@cyclicreference.space>
2017-06-03 21:14:34 -0500

    Should not be coding when tired

--- hystrix-sample/angular-project/js/components/messages.module.js
+++ hystrix-sample/angular-project/js/components/messages.module.js
@@ -1,16 +1,35 @@
 'use strict';
 
 var app = angular.module('myApp');
-app.controller('messageCtrl',["$scope", function ($scope) {
+app.controller('messageCtrl', ["$scope", function ($scope) {
     $scope.events = [];
-    $scope.init = function(){
-        var eventSource = new EventSource('/test.stream');
-        eventSource.onmessage = function(event){
-            if($scope.events.length > 10){
-                $scope.events.splice(0,1);
-            }
-            $scope.events.push(event.data);
-            $scope.$apply();
-        }
+    $scope.init = function () {
+
+        // eventSource.onmessage = function(event){
+        //     if($scope.events.length > 10){
+        //         $scope.events.splice(0,1);
+        //     }
+        //     $scope.events.push(event.data);
+        //     $scope.$apply();
+        // }
+    }
+}]);
+
+app.service('messageService', [function () {
+    var self = this;
+    self.messageObserver = Observable.create(function (observer) {
+        const eventSource = new EventSource('/interval-sse-observable');
+        eventSource.onmessage = function (x) {
+            observer.next(console.log(JSON.parse(x.data)));
+        };
+        eventSource.onerror = function (x) {
+            observer.error(console.log('EventSource failed ' + e));
+        };
+        return function () {
+            eventSource.close();
+        };
+    });
+    return {
+        messageStream: self.messageObserver
     }
 }]);
