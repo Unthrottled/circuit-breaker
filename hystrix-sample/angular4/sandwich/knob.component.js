3e271fa5f366977d6a4b83fdb844eeda655697fa
cyclic-reference <alex@cyclicreference.space>
2017-06-09 09:47:11 -0500

    On change

--- hystrix-sample/angular4/sandwich/knob.component.js
+++ hystrix-sample/angular4/sandwich/knob.component.js
@@ -13,54 +13,11 @@ var core_1 = require("@angular/core");
 require("./knob.component.htm");
 var KnobCompontent = (function () {
     function KnobCompontent() {
-        // ngAfterViewInit(): void
-        // {
-        //
-        // }
-        // onChange(event: any): void
-        // {
-        // {
-        //     event.stopPropagation();
-        //     // if (event.args.changeSource == 'propertyChange' || event.args.changeSource == 'val') { return; }
-        //     // this.numberInput.val(event.args.value);
-        // }
-        // onMouseDown(event: any): void
-        // }
-        // onKeyup(): void
-        // {
-        // }
-        // onValueChanged(): void
-        // {
-        // }
         this.poop = 10;
-        this.style = {
-            stroke: '#dfe3e9', strokeWidth: 3,
-            fill: {
-                color: '#fefefe', gradientType: "linear",
-                gradientStops: [[0, 1], [50, 0.9], [100, 1]]
-            }
-        };
-        this.marks = {
-            colorRemaining: { color: 'grey', border: 'grey' },
-            colorProgress: { color: '#00a4e1', border: '#00a4e1' },
-            type: 'line', offset: '71%', thickness: 3, size: '6%',
-            majorSize: '9%', majorInterval: 10, minorInterval: 2
-        };
-        this.labels = {
-            offset: '88%',
-            step: 10,
-            visible: true
-        };
-        this.progressBar = {
-            style: { fill: '#00a4e1', stroke: 'grey' },
-            size: '9%', offset: '60%',
-            background: { fill: 'grey', stroke: 'grey' }
-        };
-        this.pointer = {
-            type: 'arrow', style: { fill: '#00a4e1', stroke: 'grey' },
-            size: '59%', offset: '49%', thickness: 20
-        };
     }
+    KnobCompontent.prototype.onChange = function () {
+        //do things.
+    };
     return KnobCompontent;
 }());
 KnobCompontent = __decorate([
--- hystrix-sample/angular4/sandwich/knob.component.ts
+++ hystrix-sample/angular4/sandwich/knob.component.ts
@@ -10,58 +10,9 @@ import './knob.component.htm';
     templateUrl: `./templates/knob.component.htm`,
     styleUrls: []
 })
-export class KnobCompontent
-{
-    // ngAfterViewInit(): void
-    // {
-    //
-    // }
-    // onChange(event: any): void
-    // {
-    // {
-    //     event.stopPropagation();
-    //     // if (event.args.changeSource == 'propertyChange' || event.args.changeSource == 'val') { return; }
-    //     // this.numberInput.val(event.args.value);
-    // }
-    // onMouseDown(event: any): void
-    // }
-    // onKeyup(): void
-    // {
-    // }
-    // onValueChanged(): void
-    // {
-    // }
+export class KnobCompontent {
+    onChange(): void {
+        //do things.
+    }
     poop = 10;
-    style: any =
-        {
-            stroke: '#dfe3e9', strokeWidth: 3,
-            fill: {
-                color: '#fefefe', gradientType: "linear",
-                gradientStops: [[0, 1], [50, 0.9], [100, 1]]
-            }
-        };
-    marks: any =
-        {
-            colorRemaining: { color: 'grey', border: 'grey' },
-            colorProgress: { color: '#00a4e1', border: '#00a4e1' },
-            type: 'line', offset: '71%', thickness: 3, size: '6%',
-            majorSize: '9%', majorInterval: 10, minorInterval: 2
-        };
-    labels: any =
-        {
-            offset: '88%',
-            step: 10,
-            visible: true
-        };
-    progressBar: any =
-        {
-            style: { fill: '#00a4e1', stroke: 'grey' },
-            size: '9%', offset: '60%',
-            background: { fill: 'grey', stroke: 'grey' }
-        };
-    pointer: any =
-        {
-            type: 'arrow', style: { fill: '#00a4e1', stroke: 'grey' },
-            size: '59%', offset: '49%', thickness: 20
-        };
 }
